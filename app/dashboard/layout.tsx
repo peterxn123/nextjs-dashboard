5e9e182c681146da8496a82b4a9686e8abde9a14
peterxn123 <125126638+peterxn123@users.noreply.github.com>
2025-02-15 18:02:48 +0000

    Added new features - improved dashboard and invoices page including search function

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
@@ -8,5 +9,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
+
     );
 }
